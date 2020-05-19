7ae0eae06385c8634b78f2da8aeb7b7f6540d2ec
Renata Kulcsar <reny_kulcsar@yahoo.com>
2020-05-19 19:20:28 +0300

    Reny2

--- src/main/java/org/loose/fis/registration/example/controllers/ReaderHomePageController.java
+++ src/main/java/org/loose/fis/registration/example/controllers/ReaderHomePageController.java
@@ -7,20 +7,21 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import org.loose.fis.registration.example.Main;
 import org.loose.fis.registration.example.classes.Author;
 import org.loose.fis.registration.example.classes.Book;
 
 import java.util.ArrayList;
 
 public class ReaderHomePageController {
+    Stage stage = Main.getStage();
+
     private ArrayList<Book> Favorites;
     private ArrayList<Book> CurrentlyReading;
     private ArrayList<Book> WantToRead;
     private ArrayList<Book> FinishedBooks;
     private ArrayList<Author> followedAuthors;
     private ArrayList<String> quotes;
-
-    Stage homePage = new Stage();
 
     public ReaderHomePageController() {
         Favorites = new ArrayList<Book>();
@@ -29,17 +30,6 @@ public class ReaderHomePageController {
         FinishedBooks = new ArrayList<Book>();
         followedAuthors = new ArrayList<Author>();
         quotes = new ArrayList<String>();
-
-        StackPane layout = new StackPane();
-        Image wallpaper_image = new Image("https://bgwall.net/wp-content/uploads/2014/09/books-at-the-library-wallpaper-picture.jpg");
-        BackgroundImage bi = new BackgroundImage(wallpaper_image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
-                BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-        Background background = new Background(bi);
-        layout.setBackground(background);
-
-        Scene scene = new Scene(layout, 500, 500);
-        homePage.setScene(scene);
-        homePage.show();
     }
 
     public void addBookToList(Book b, ArrayList<Book> a){
@@ -78,8 +68,8 @@ public class ReaderHomePageController {
             list.getItems().add(button);
         }
         Scene s = new Scene(list, 500, 500);
-        this.homePage.setScene(s);
-        this.homePage.show();
+        stage.setScene(s);
+        stage.show();
     }
 
     public void handleCurrentlyReading(){
@@ -90,8 +80,8 @@ public class ReaderHomePageController {
             list.getItems().add(button);
         }
         Scene s = new Scene(list, 500, 500);
-        this.homePage.setScene(s);
-        this.homePage.show();
+        stage.setScene(s);
+        stage.show();
     }
 
     public void handleWantToRead(){
@@ -102,8 +92,8 @@ public class ReaderHomePageController {
             list.getItems().add(button);
         }
         Scene s = new Scene(list, 500, 500);
-        this.homePage.setScene(s);
-        this.homePage.show();
+        stage.setScene(s);
+        stage.show();
     }
 
     public void handleFinishedReading(){
@@ -114,8 +104,8 @@ public class ReaderHomePageController {
             list.getItems().add(button);
         }
         Scene s = new Scene(list, 500, 500);
-        this.homePage.setScene(s);
-        this.homePage.show();
+        stage.setScene(s);
+        stage.show();
     }
 
     public void handleAuthorsList(){
@@ -125,8 +115,8 @@ public class ReaderHomePageController {
             list.getItems().add(button);
         }
         Scene s = new Scene(list, 500, 500);
-        this.homePage.setScene(s);
-        this.homePage.show();
+        this.stage.setScene(s);
+        this.stage.show();
     }
 
     public void handleQuoteList(){
@@ -135,8 +125,8 @@ public class ReaderHomePageController {
             list.getItems().add(s);
         }
         Scene s = new Scene(list, 500, 500);
-        this.homePage.setScene(s);
-        this.homePage.show();
+        this.stage.setScene(s);
+        this.stage.show();
     }
 
     public void followAuthor(Author a){
